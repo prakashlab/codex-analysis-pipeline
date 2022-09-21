d4d295195f8fdde3751620c182622a62202ee9e2
Kevin Marx <kevinmarx14@gmail.com>
2022-09-21 12:50:01 -0700

    improved training with m2unet

--- train_native.py
+++ train_native.py
@@ -14,21 +14,21 @@ import torch
 
 def main():
     # setting up
-    data_dir = '/media/prakashlab/T7' # data should contain a train and a test folder
-    model_root = "./models"
-    epochs = 1600
+    data_dir = '/media/prakashlab/T7/subsets'#'./Cellpose Exports for 30deg 4ul 15mmps' # data should contain a train and a test folder
+    model_root = "./models3"
+    epochs = 2000
     steps = 1
-    resume = False
+    resume = True
     corrid = "200"
     pretrained_model = None # os.path.join(model_root, str(corrid), "model.h5")
 
     # define the transforms
     transform = A.Compose(
         [
-            A.RandomCrop(363, 363),
-            A.Rotate(limit=360, p=1),
-            A.HorizontalFlip(p=1),
-            A.CenterCrop(256, 256),        
+            A.RandomCrop(1500, 1500),
+            A.Rotate(limit=(-10, 10), p=1),
+            A.HorizontalFlip(p=0.5),
+            A.CenterCrop(1024, 1024),  
         ]
     )
     # unet model hyperparamer can be found here: https://notebooks.githubusercontent.com/view/ipynb?browser=chrome&color_mode=auto&commit=f899f7a8a9144b3f946c4a1362f7e38ae0c00c59&device=unknown_device&enc_url=68747470733a2f2f7261772e67697468756275736572636f6e74656e742e636f6d2f79696e676b61697368612f6b657261732d756e65742d636f6c6c656374696f6e2f663839396637613861393134346233663934366334613133363266376533386165306330306335392f6578616d706c65732f757365725f67756964655f6d6f64656c732e6970796e62&logged_in=true&nwo=yingkaisha%2Fkeras-unet-collection&path=examples%2Fuser_guide_models.ipynb&platform=mac&repository_id=323426984&repository_type=Repository&version=95#Swin-UNET
@@ -47,7 +47,7 @@ def main():
 
 def perform_training(data_dir, model_root, epochs, steps, resume, corrid, pretrained_model, transform, model_config):
     os.makedirs(os.path.join(model_root, str(corrid)), exist_ok=True)
-    A.save(transform, "./models/transform.json")
+    A.save(transform, model_root + "/transform.json")
 
     # check if GPU is available
     print(f'GPU: {torch.cuda.is_available()}')
@@ -83,7 +83,7 @@ def perform_training(data_dir, model_root, epochs, steps, resume, corrid, pretra
 
 # test
 def perform_testing(data_dir, model_config, model_root, resume, pretrained_model, corrid):
-    test_samples = load_samples(data_dir + '/train')
+    test_samples = load_samples(data_dir + '/test')
     model = M2UnetInteractiveModel(
         model_config=model_config,
         model_dir=model_root,
@@ -92,18 +92,18 @@ def perform_testing(data_dir, model_config, model_root, resume, pretrained_model
         default_save_path=os.path.join(model_root, str(corrid), "model.pth"),
     )
     for i, sample in enumerate(test_samples):
-        inputs = sample[0].astype("float32")[None, :256, :256, :]
-        imageio.imwrite(f"octopi-inputs_{i}.png", inputs[0].astype('uint8'))
+        inputs = sample[0].astype("float32")[None, :1024, :1024, :]
+        imageio.imwrite(f"m2unet/octopi-inputs_{i}.png", inputs[0].astype('uint8'))
         inputs = (inputs - np.mean(inputs)) /np.std(inputs)
-        labels = sample[1].astype("float32")[None, :256, :256, :] * 255
-        imageio.imwrite(f"octopi-labels_{i}.png", labels[0].astype('uint8'))
+        labels = sample[1].astype("float32")[None, :1024, :1024, :] * 255
+        imageio.imwrite(f"m2unet/octopi-labels_{i}.png", labels[0].astype('uint8'))
         results = model.predict(inputs)
         output = np.clip(results[0] * 255, 0, 255)[:, :, 0].astype('uint8')
-        imageio.imwrite(f"octopi-pred-prob_{i}.png", output)
+        imageio.imwrite(f"m2unet/octopi-pred-prob_{i}.png", output)
         threshold = threshold_otsu(output)
         mask = ((output > threshold) * 255).astype('uint8')
         predict_labels = label(mask)
-        imageio.imwrite(f"octopi-pred-labels_{i}.png", predict_labels)
+        imageio.imwrite(f"m2unet/octopi-pred-labels_{i}.png", predict_labels)
 
     print("all done")
 
