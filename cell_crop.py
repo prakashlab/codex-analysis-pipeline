8de68419865ba73fbda465fc0650be0063ca4a01
Kevin Marx <kevinmarx14@gmail.com>
2022-11-17 13:26:56 -0800

    cell crop rev2 finished - todo: make remote work better

--- cell_crop.py
+++ cell_crop.py
@@ -11,19 +11,20 @@ def main():
     # root_dir needs a trailing slash (i.e. /root/dir/)
     root_dir = "gs://octopi-codex-data/"
     exp_id   = "20220823_20x_PBMC_2/"
-    dest_dir = "/home/prakashlab/Documents/kmarx/" + exp_id + "image_crop/"
+    dest_dir = "gs://octopi-codex-data-processing/foF3pmoJguvzNdwqbUmpOkwRzAsv39FO/" + exp_id + "image_crop/"
     channel =  "Fluorescence_405_nm_Ex" # only crop images from this channel
     celltype_file = "./08_23_22_PBMC_Octopi_celltypes.csv"
     zstack  = 0 # select which z to crop. set to 'f' to select the focus-stacked
     key = "/home/prakashlab/Documents/fstack/codex-20220324-keys.json"
     gcs_project = 'soe-octopi'
     cell_radius = 30
+    n_of_each_type = 200
     t0 = time.time()
-    make_crops(root_dir, exp_id, channel, zstack, celltype_file, key, gcs_project, dest_dir, cell_radius)
+    make_crops(root_dir, exp_id, channel, zstack, celltype_file, key, gcs_project, dest_dir, cell_radius, n_of_each_type)
     t1 = time.time()
     print(t1-t0)
 
-def make_crops(root_dir, exp_id, channel, zstack, celltype_file, key, gcs_project, dest_dir, cell_radius):
+def make_crops(root_dir, exp_id, channel, zstack, celltype_file, key, gcs_project, dest_dir, cell_radius, n_of_each_type):
     img_remote = False
     if root_dir[0:5] == 'gs://':
         img_remote = True
@@ -31,7 +32,7 @@ def make_crops(root_dir, exp_id, channel, zstack, celltype_file, key, gcs_projec
     if celltype_file[0:5] == 'gs://':
         data_remote = True
     dest_remote = False
-    dest_local = dest_remote
+    dest_local = dest_dir
     if dest_dir[0:5] == 'gs://':
         dest_local = "./imgs_"+exp_id
         dest_remote = True
@@ -48,12 +49,34 @@ def make_crops(root_dir, exp_id, channel, zstack, celltype_file, key, gcs_projec
         with open(celltype_file, 'r' ) as f:
             celltype_df = pd.read_csv(celltype_file)
     
+    df = None
+    path = root_dir + exp_id + 'index.csv'
+    if img_remote:
+        with fs.open(path, 'r' ) as f:
+            df = pd.read_csv(f)
+    else:
+        with open( path, 'r' ) as f:
+            df = pd.read_csv(f)
+    id = df.loc[0, 'Acquisition_ID']
+    print(id)
+
+    seen_types = dict()
+    
     for i in range(celltype_df.shape[0]):
         i_idx = int(celltype_df["i"][i])
         j_idx = int(celltype_df["j"][i])
-        xpos  = int(celltype_df["x"][i])
-        ypos  = int(celltype_df["y"][i])
-        id = 'cycle0_2022-08-23_20-15-33.401781'
+        ypos  = int(celltype_df["x"][i])
+        xpos  = int(celltype_df["y"][i])
+        cell_type = celltype_df["cell_type"][i]
+
+        # check if we have seen this cell type already
+        if cell_type in seen_types.keys():
+            if seen_types[cell_type] >= n_of_each_type:
+                continue
+            else:
+                seen_types[cell_type] += 1
+        else:
+            seen_types[cell_type] = 1
         # load image 
         filename = id + '/0/' + str(i_idx) + '_' + str(j_idx) + '_' + str(zstack) + '_' + channel + '.bmp'
         image_path = root_dir + exp_id + filename
@@ -71,12 +94,17 @@ def make_crops(root_dir, exp_id, channel, zstack, celltype_file, key, gcs_projec
         cropped_image = im[xcrop[0]:xcrop[1], ycrop[0]:ycrop[1]]
 
         cell_type = celltype_df["cell_type"][i]
-        filename = cell_type + str(i_idx) + '_' + str(j_idx) + '_' + str(zstack) + '_' + channel + '.bmp'
-        savepath = dest_dir + exp_id + id + '/0/'
+        filename = str(ypos) + "_" + str(xpos) + "_" + str(i_idx) + '_' + str(j_idx) + '_' + str(zstack) + '_' + channel + '.bmp'
+        savepath = dest_local + '/' + cell_type + '/'
         os.makedirs(savepath, exist_ok=True)
         savepath = savepath + filename
         print(savepath)
+        remotepath = dest_dir + cell_type + '/' + filename
+        print(remotepath)
         cv2.imwrite(savepath, cropped_image)
+        if dest_remote:
+            fs.put(savepath, remotepath)
+            os.remove(savepath)
 
 
 
